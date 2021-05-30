b1e19a92667e5c454a7be5eb26bd10f3fc1df0c0
Nuha <nahuelalderete08@gmail.com>
2021-05-29 22:27:58 -0300

    Se añadieron nuevos ejercicios

---
+++ Ejercicios-Nahuel/Ejercicio1.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+
+cout<<"Meju Army"<<endl;
+int a=2;
+int b;
+int c;
+cout<<"Inserte el valor del numero a:"<<endl;
+cin>>a;
+cout<<"Inserte el valor del numero b:"<<endl;
+cin>>b;
+cout<< a*b<<endl;
+cout<<"el valor es "<<endl;
+cout<<a+b;
+if (b==0)
+{cout<<"No se divide entre 0";
+    /* code */
+}else{
+    cout<<a/b<<endl;
+}
+
+bool Mejunje=false;
+if (Mejunje)
+{
+    cout<<"es un sisinene";
+}else{
+    cout<<"tkm";
+}
+
+cout<<endl<<endl<<"ola"<<endl;
+if (a>b)
+{cout<<"A es mayor que B";
+    
+}else{cout<<"A no es mayor que B";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//hola xd
+//int a; ----> número entero
+//int a = 3; número entero pero con un valor asignado
+// ;   ----> fin de la secuencia
+//en c++ ecxisten distintos tipos de variables
+//una variable es por ejemplo "a" que es un entero
+/*Los tipos de variables que existen son:
+int --> entero
+float ----> número flotante (con coma) (se le llama flotante porque tiene una particularidad a nivel código máquina)
+string ---> cadena de caracteres "hola buenas tardes."
+bool ----> indica verdadero o falso (true o false)
+*/
+//no te compliques negro, es mucha información, pero en los vídeos empiezan tranqui
+//tampoco hay que centrarse mucho en los puntos y comas o cómo escribimos (es importante, pero con el tiempo uno aprende a identificar)
+
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ Ejercicios-Nahuel/Repaso calculadora.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+cout<<"Mejunje"<<endl;
+
+
+float a;
+float b;
+
+cout<<"inserta el valor meju"<<endl;
+cin>>a;
+cout<<"inserta el otro valor meju"<<endl;
+cin>>b;
+ 
+ cout<<"el valor del meju es"<<endl;
+ cout<<a+b;
+
+
+
+
+
+
+
+}
