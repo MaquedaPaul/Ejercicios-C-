e619b15b035590dc5192ed278d19074ac94f5e11
Nuha <nahuelalderete08@gmail.com>
2021-05-29 23:14:00 -0300

    Vectores

--- Ejercicios-Nahuel/Holamundo.cpp
+++ Ejercicios-Nahuel/Holamundo.cpp
@@ -5,4 +5,9 @@ using namespace std;
 int main(){
     cout<<"Hola mundo"<<endl;
     return 0;
+// git add .<< para reportar
+// git status<< para ver si reportaron
+// git commit -m "Se añadieron nuevos ejercicios"
+//git push<< guardar en la nube
+
 }
---
+++ Repasos/Vectores.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "hola";
+    int vector [100];
+    vector[0] = 5;
+    vector[77] = -152;
+    cout << vector[0] << endl;
+    cout << vector[77] << endl;
+    cout << vector[1] << endl;
+    int numero;
+    string hola;
+            cin >> hola;
+    return 0;
+}
+
+
