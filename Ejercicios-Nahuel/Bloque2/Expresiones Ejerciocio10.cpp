66b921a897bfbd910c76d2e79615e98aeba789f5
Nuha <nahuelalderete08@gmail.com>
2021-06-09 19:00:45 -0300

    Nuevo seteo

--- Ejercicios-Nahuel/Bloque2/Expresiones Ejerciocio10.cpp
+++ Ejercicios-Nahuel/Bloque2/Expresiones Ejerciocio10.cpp
@@ -16,16 +16,13 @@ cin>>b;
 cout<<"Digite el valor de c: ";
 cin>>c;
 
-resultado1 = (-b+sqrt(pow(b,2)-4*a*c))/(2*a);
-resultado2 = (-b-sqrt(pow(b,2)-4*a*c))/(2*a);
+resultado1 = (-b+sqrt(pow(b,2)+4*a*c))/(2*a);
+resultado2 = (-b-sqrt(pow(b,2)+4*a*c))/(2*a);
 
-cout<<"El el primer resultado es: "<<resultado1<<endl;
+cout<<"El primer resultado es: "<<resultado1<<endl;
 cout<<"El segundo resultado es : "<<resultado2<<endl;
 
 
 
-
-
-
     return 0;
-}
+} 
