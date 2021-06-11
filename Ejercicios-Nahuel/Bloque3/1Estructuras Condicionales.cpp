7cdf72a3053c789e8e454df64b3021271986f643
Nuha <nahuelalderete08@gmail.com>
2021-06-11 01:50:55 -0300

    Bloque 3 condicionales

---
+++ Ejercicios-Nahuel/Bloque3/1Estructuras Condicionales.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+/*if(condicion){
+    instrucciones 1;
+}
+else{
+    intrucciones 2;
+}*/
+
+
+
+int main(){
+   int numero, dato = 5;
+
+cout<<"Digite el valor del numero: ";
+cin>>numero;
+
+if(numero==dato){
+    cout<<"El numero es mayor que 5";
+}
+else{
+cout<<"El numero es menor a 5";    
+}
+
+// != << es diferente
+//== << es igual
+// <= << es menos o igual
+
+
+
+    return 0;
+}
