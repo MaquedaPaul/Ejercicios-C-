22b37f9a4ecb52ff52898fbaf386c811cf2827c1
Nuha <nahuelalderete08@gmail.com>
2021-06-15 20:33:10 -0300

    Ejercicio 5

---
+++ Ejercicios-Nahuel/Bloque3/4Ejerciocio.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int numero;
+    cout<<"Digite un numero: ";
+    cin>>numero;
+
+    if(numero==0){
+        cout<<"El numero es 0";
+    }
+else if(numero>0){
+    cout<<"El numero es positivo";
+}
+    else{
+        cout<<"el numero es negativo";
+    }
+
+
+    return 0;
+}
